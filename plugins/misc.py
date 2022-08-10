0a6baa28afc4f0bdfbaca36de823f3c7217f73ca
rogertech77 <83303776+rogertech77@users.noreply.github.com>
2022-08-10 11:48:06 +0300

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -161,8 +161,8 @@ async def imdb_callback(bot: Client, quer_y: CallbackQuery):
             ],
             [
                 InlineKeyboardButton(
-                    text=f"Projects Channel",
-                    url="https://t.me/josprojects"
+                    text=f"Msaada",
+                    url="https://t.me/kibandainfo"
                 )
             ],
         ]
