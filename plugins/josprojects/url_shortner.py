07461e5e9a36deda0b51f3588aecbfb8f32ca0d9
rogertech77 <83303776+rogertech77@users.noreply.github.com>
2022-08-10 10:35:23 +0300

    Update url_shortner.py

--- plugins/josprojects/url_shortner.py
+++ plugins/josprojects/url_shortner.py
@@ -27,7 +27,7 @@ GPLINKS_API = os.environ.get("GPLINKS_API", "008ccaedd6061ad1948838f410947603de9
 
 reply_markup = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton(text='join projects channel', url='https://telegram.me/josprojects')
+        InlineKeyboardButton(text='Msaada', url='https://t.me/kibandainfo')
         ]]
     )
 
@@ -190,7 +190,7 @@ async def short(link):
     
     # Send the text
     try:
-        shorten_urls += "\n\nmade by @josprojects | @jospsupport 🔥"
+        shorten_urls += "\n\n Imetengenezwa👨🏽‍💻 na @Rogerofficialtech | Instagram 🤖 - @rogerofficialtech_tz🔥"
         return shorten_urls
     except Exception as error:
         return error
